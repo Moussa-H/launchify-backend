7dbfecd7230b9b4e5374d1e0764ba1ad9d955689
Moussa_Haidar <moussahaidar999@gmail.com>
2024-08-26 23:03:57 +0300

    Add create user method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,4 +15,29 @@ class UserController extends Controller
         // Return the users as a JSON response
         return response()->json($users);
     }
+
+   public function store(Request $request)
+{
+    // Validate the request data
+    $request->validate([
+        'username' => 'required|unique:users|max:255',
+        'email' => 'required|unique:users|email|max:255',
+        'password' => 'required|min:6',
+        'role' => 'required|in:startup,investor,mentor',
+    ]);
+
+    // Create a new user
+    $user = User::create([
+        'username' => $request->input('username'),
+        'email' => $request->input('email'),
+        'password' => bcrypt($request->input('password')), // Encrypt the password
+        'role' => $request->input('role'),
+    ]);
+
+    // Return the created user as a JSON response
+    return response()->json($user, 201);
+}
+
+
+
 }
