991879c2baba7fdb114a8114e77783a591d2629f
Moussa_Haidar <moussahaidar999@gmail.com>
2024-08-30 02:33:21 +0300

    creating a migration for the startup_investment_sources table

---
+++ database/migrations/2024_08_29_233247_create_startup_investment_sources_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('startup_investment_sources', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('startup_id')->constrained('startups')->onDelete('cascade');
+            $table->enum('investment_source', ['Business Angel', 'Public grant', 'Accelerator', 'Corporate', 'VC Fund', 'Crowd']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('startup_investment_sources');
+    }
+};
