f5d280e2dc094ab7c4d2be04df2801a564f59eea
Moussa_Haidar <moussahaidar999@gmail.com>
2024-08-30 14:44:05 +0300

    creating a model for the sector

---
+++ app/Models/Sector.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sector extends Model
+{
+    use HasFactory;
+
+
+     protected $fillable = ['id', 'name'];
+
+    public function startups()
+    {
+        return $this->belongsToMany(Startup::class, 'startup_sectors');
+    }
+}
