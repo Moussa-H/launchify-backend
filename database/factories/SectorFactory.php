8dc94f9ae11be9da7532ab2ba874d0e2b4f3f853
Moussa_Haidar <moussahaidar999@gmail.com>
2024-08-30 15:01:18 +0300

    creating a SectorFactory

---
+++ database/factories/SectorFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Sector;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sector>
+ */
+class SectorFactory extends Factory
+{
+
+      protected $model = Sector::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => $this->faker->unique()->numberBetween(1, 60),
+            'name' => $this->faker->word,
+        ];
+    }
+}
