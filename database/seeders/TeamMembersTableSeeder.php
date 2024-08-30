6dd340f814dfa1d691d19e925efc60dbee1d16c9
Moussa_Haidar <moussahaidar999@gmail.com>
2024-08-30 15:44:30 +0300

    creating a TeamMembersTableSeeder

---
+++ database/seeders/TeamMembersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\TeamMember;
+class TeamMembersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+   {
+        TeamMember::create([
+            'startup_id' => 1,
+            'fullname' => 'Jane Smith',
+            'job_title' => 'CTO',
+            'salary' => 120000
+        ]);
+
+        TeamMember::create([
+            'startup_id' => 1,
+            'fullname' => 'Alice Johnson',
+            'job_title' => 'Lead Developer',
+            'salary' => 100000
+        ]);
+    }
+}
