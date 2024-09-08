4f5abb66a4e2e352ff19c8f45b448f4c2ef2a280
Moussa_Haidar <moussahaidar999@gmail.com>
2024-09-09 02:25:05 +0300

    creating function store in the ExpenseController

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -17,7 +17,26 @@ class IncomeController extends Controller
         return response()->json($incomes);
     }
 
-   
+    // Store a new income
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'startup_id' => 'required|exists:startups,id',
+            'product_sales' => 'required|integer',
+            'service_revenue' => 'required|integer',
+            'subscription_fees' => 'required|integer',
+            'investment_income' => 'required|integer',
+            'year' => 'required|integer',
+            'month' => 'required|integer|min:1|max:12',
+        ]);
+
+        $income = Income::updateOrCreate(
+            ['startup_id' => $validatedData['startup_id'], 'year' => $validatedData['year'], 'month' => $validatedData['month']],
+            $validatedData
+        );
+
+        return response()->json($income, 201);
+    }
 
   
 
