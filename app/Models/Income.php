bd4848f6ce023db167d3e7f09e1a82f6c979cf2d
Moussa_Haidar <moussahaidar999@gmail.com>
2024-09-09 02:00:28 +0300

    creating a model for income

---
+++ app/Models/Income.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Income extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'startup_id', 
+        'product_sales', 
+        'service_revenue', 
+        'subscription_fees', 
+        'investment_income'
+    ];
+
+    public function startup()
+    {
+        return $this->belongsTo(Startup::class);
+    }
+}
