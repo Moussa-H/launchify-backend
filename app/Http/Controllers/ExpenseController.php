37bdadd46ea2dfe8127a6e91008f318e5a04b26c
Moussa_Haidar <moussahaidar999@gmail.com>
2024-09-09 02:21:29 +0300

    creating function store in the ExpenseController

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -17,5 +17,27 @@ class ExpenseController extends Controller
         return response()->json($expenses);
     }
 
-   
+     public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'startup_id' => 'required|exists:startups,id',
+            'office_rent' => 'required|integer',
+            'marketing' => 'required|integer',
+            'legal_accounting' => 'required|integer',
+            'maintenance' => 'required|integer',
+            'software_licenses' => 'required|integer',
+            'office_supplies' => 'required|integer',
+            'miscellaneous' => 'required|integer',
+            'year' => 'required|integer',
+            'month' => 'required|integer|min:1|max:12',
+        ]);
+
+        $expense = Expense::updateOrCreate(
+            ['startup_id' => $validatedData['startup_id'], 'year' => $validatedData['year'], 'month' => $validatedData['month']],
+            $validatedData
+        );
+
+        return response()->json($expense, 201);
+    }
+
 }
