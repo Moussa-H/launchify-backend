e0827d75734a686634c212aeca73cce8b4664a5b
Moussa_Haidar <moussahaidar999@gmail.com>
2024-09-09 03:28:42 +0300

    creating function store  in the TeamMemberController

--- app/Http/Controllers/TeamMemberController.php
+++ app/Http/Controllers/TeamMemberController.php
@@ -17,7 +17,26 @@ class TeamMemberController extends Controller
         return response()->json($teamMembers);
     }
 
-  
+    // Store a new team member
+    public function store(Request $request, $startupId)
+    {
+        $startup = Startup::findOrFail($startupId);
+
+        $validatedData = $request->validate([
+            'fullname' => 'required|string|max:255',
+            'position' => 'required|string|max:255',
+            'salary' => 'required|integer',
+        ]);
+
+        $teamMember = TeamMember::create([
+            'startup_id' => $startupId,
+            'fullname' => $validatedData['fullname'],
+            'position' => $validatedData['position'],
+            'salary' => $validatedData['salary'],
+        ]);
+
+        return response()->json($teamMember, 201);
+    }
 
   
 
