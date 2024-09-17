b834681a7a98e3ff5370729b4b57bc7e91297f77
Moussa_Haidar <moussahaidar999@gmail.com>
2024-09-18 02:26:16 +0300

    Creating a model Strategy

--- app/Models/Strategy.php
+++ app/Models/Strategy.php
@@ -9,8 +9,27 @@ class Strategy extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['startup_id', 'title', 'description', 'actionable_steps', 'potential_challenges'];
+    // Allow mass assignment for these fields
+    protected $fillable = [
+        'startup_id',
+        'strategy_1_name',
+        'strategy_1_description',
+        'strategy_1_status',
+        'strategy_2_name',
+        'strategy_2_description',
+        'strategy_2_status',
+        'strategy_3_name',
+        'strategy_3_description',
+        'strategy_3_status',
+        'strategy_4_name',
+        'strategy_4_description',
+        'strategy_4_status',
+        'strategy_5_name',
+        'strategy_5_description',
+        'strategy_5_status',
+    ];
 
+    // Define the relationship with the Startup model
     public function startup()
     {
         return $this->belongsTo(Startup::class);
