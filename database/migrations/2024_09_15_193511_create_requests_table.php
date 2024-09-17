54c5e30c6fd234c8691645fb294bcb65541e5a40
Moussa_Haidar <moussahaidar999@gmail.com>
2024-09-18 01:47:10 +0300

    Creating a migration requests_table

---
+++ database/migrations/2024_09_15_193511_create_requests_table.php
@@ -0,0 +1,35 @@
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('startup_id');
+            $table->unsignedBigInteger('mentor_id');
+            $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
+            $table->timestamps();
+
+            // Foreign key constraints
+            $table->foreign('startup_id')->references('id')->on('startups')->onDelete('cascade');
+            $table->foreign('mentor_id')->references('id')->on('mentors')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
