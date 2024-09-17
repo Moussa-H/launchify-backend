1ede5d8ad36995b3394dd93e8ad57df5f02358a3
Moussa_Haidar <moussahaidar999@gmail.com>
2024-09-18 01:17:11 +0300

    Creating functions index, show in the Investor Controller

---
+++ app/Http/Controllers/InvestorController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\Investor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class InvestorController extends Controller
+{
+    public function index()
+    {
+        return Investor::all();
+    }
+
+    public function show($id)
+    {
+        $investor = Investor::find($id);
+
+        if (!$investor) {
+            return response()->json(['message' => 'Investor not found'], 404);
+        }
+
+        return response()->json($investor, 200);
+    }
+
+
+
+
+
+
+
+
+}
